7119a5f573abf57ff9f40a91bf200cfb4e1e5b1f
Josiandro <josiandro.santossilveira@hotmail.com>
2023-02-14 07:39:24 -0300

    Laços de repetição

---
+++ Java_JRE_e_JDK/sintaxe-variavies-e-fluxo/src/MultiplosDe3ComFor.java
@@ -0,0 +1,9 @@
+
+public class MultiplosDe3ComFor {
+	public static void main(String[] args) {
+		
+		for(int contador = 3; contador <= 100; contador += 3) {
+			System.out.print(contador + " ");
+		}
+	}
+}
---
+++ Java_JRE_e_JDK/sintaxe-variavies-e-fluxo/src/MultiplosDe3ComIf.java
@@ -0,0 +1,13 @@
+
+public class MultiplosDe3ComIf {
+	public static void main(String[] args) {
+		
+		for(int contador = 1; contador <= 100; contador++) {
+			
+			if (contador % 3 == 0) {
+				System.out.print(contador + " ");
+			}
+		
+		}
+	}
+}
