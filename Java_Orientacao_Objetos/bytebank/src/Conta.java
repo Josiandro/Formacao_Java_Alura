fb9881bbcbf0c83f1fa7de0eb0109034ed41d416
Josiandro <josiandro.santossilveira@hotmail.com>
2023-02-22 06:08:13 -0300

    Primeira Instância

---
+++ Java_Orientacao_Objetos/bytebank/src/Conta.java
@@ -0,0 +1,7 @@
+// Especificação do tipo Conta com seus atributos
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+}
---
+++ Java_Orientacao_Objetos/bytebank/src/CriaConta.java
@@ -0,0 +1,10 @@
+// Objeto/Instância do tipo conta
+public class CriaConta {
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		
+		primeiraConta.saldo = 200;
+		System.out.print(primeiraConta.saldo);
+	}
+}
